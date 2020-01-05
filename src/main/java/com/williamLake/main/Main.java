36675603b6b77ff7340ec0785a3226bb4e12e561
William Lake <william.j.lake2000@gmail.com>
2020-01-05 18:05:17 +0000

    Test for numOfBellsValid created

--- src/main/java/com/williamLake/main/Main.java
+++ src/main/java/com/williamLake/main/Main.java
@@ -8,20 +8,20 @@ public class Main {
 	// write your code here
     }
 
-    public static int NumberOfBells(){
+    public static int numberOfBells(){
         int numOfBells = 0;
         boolean valid = false;
         while (valid == false){
             System.out.print("How many bells in the methord (Enter number between 4 and 12): ");
-            numOfBells = GetIntInput();
-            if (CheckNumOfBellsValid((numOfBells))){
+            numOfBells = getIntInput();
+            if (numOfBellsValid((numOfBells))){
                 valid = true;
             }
         }
         return numOfBells;
     }
 
-    public static boolean CheckNumOfBellsValid(int numOfBells){
+    public static boolean numOfBellsValid(int numOfBells){
         if (numOfBells >= 4 && numOfBells <= 12){
             return true;
         } else{
@@ -30,7 +30,7 @@ public class Main {
         }
     }
 
-    private static int GetIntInput(){
+    private static int getIntInput(){
         Scanner keyboard = new Scanner(System.in);
         return keyboard.nextInt();
     }
--- src/test/java/com/williamlake/main/MainTest.java
+++ src/test/java/com/williamlake/main/MainTest.java
@@ -2,10 +2,35 @@ package com.williamlake.main;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MainTest {
 
     @Test
-    void myTest() {
+    public void numOfBellsValidCorrectInputTest() {
+        int[] numOfBells = new int[3];
+        Boolean result;
+        numOfBells[0] = 4;
+        numOfBells[1] = 8;
+        numOfBells[2] = 12;
 
+        for (int i = 0; i<=2; i++){
+            result = Main.numOfBellsValid(numOfBells[i]);
+            assertEquals(true,result);
+        }
+    }
+
+    @Test
+    public void numOfBellsValidIncorrectInputTest() {
+        int[] numOfBells = new int[3];
+        Boolean result;
+        numOfBells[0] = 3;
+        numOfBells[1] = 13;
+        numOfBells[2] = 100;
+
+        for (int i = 0; i<=2; i++){
+            result = Main.numOfBellsValid(numOfBells[i]);
+            assertEquals(false,result);
+        }
     }
 }
